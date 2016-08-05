b70f749779f07b5a78b744e4fbd9ef8252da0494
Simon Frei <freisim93@gmail.com>
2016-08-06 01:22:27 +0200

    Create necessary files for python packaging

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name="kodistubs",
+    version='1.0.0',
+    description="Recreated kodi modules as stubs with docstrings.",
+    author="romanvm",
+    url="https://github.com/imsodin/Kodistubs",
+    license="GPLv3",
+    keywords="kodi documentation inspection",
+    packages=['.'],
+    classifieres=[
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3'
+        ]
+)
