1678fa05e5bb808dba4bd18863bfa6e15add2fa5
Roman Miroshnychenko <roman1972@gmail.com>
2018-11-29 08:54:41 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,30 @@
+# coding: utf-8
+
 from io import open
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
-import kodistubs_meta
+
+VERSION = '18.0.0'
+AUTHOR = 'Roman Miroshnychenko'
+EMAIL = 'roman1972@gmail.com'
 
 with open('README.rst', encoding='utf-8') as fo:
     long_descr = fo.read()
 
 setup(
     name='Kodistubs',
-    version=kodistubs_meta.VERSION,
-    py_modules=['xbmc', 'xbmcaddon', 'xbmcgui', 'xbmcplugin', 'xbmcvfs'],
+    version=VERSION,
+    py_modules=[
+        'xbmc', 'xbmcaddon', 'xbmcgui', 'xbmcplugin', 'xbmcvfs', 'xbmcdrm'
+    ],
     install_requires=['typing'],
     zip_safe=False,
     description='Stub modules that re-create Kodi Python API',
     long_description=long_descr,
-    author=kodistubs_meta.AUTHOR,  # The new Kodistubs have been generated from scratch
-    author_email=kodistubs_meta.EMAIL,
+    author=AUTHOR,  # The new Kodistubs have been generated from scratch
+    author_email=EMAIL,
     url='https://github.com/romanvm/Kodistubs',
     license='GPLv3',
     keywords="kodi documentation inspection",
