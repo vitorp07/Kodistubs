6f59bf2b12b9d3e6e8e2fd627a566632191c3be7
Roman Miroshnychenko <roman1972@gmail.com>
2018-12-02 17:45:19 +0200

    Update xbmcaddon module

--- xbmcaddon.py
+++ xbmcaddon.py
@@ -16,8 +16,8 @@ class Addon(object):
     """
     Kodi's addon class
 
-    Offers classes and functions that manipulate the add-on settings,
-    information and localization.
+    Offers classes and functions that manipulate the add-on settings, information
+    and localization.
 
     Creates a new AddOn class.
 
@@ -26,16 +26,13 @@ class Addon(object):
     Specifying the addon id is not needed. Important however is that the addon
     folder has the same name as the AddOn id provided in addon.xml.
     You can optionally specify the addon id from another installed addon
-    to retrieve settings from it.
-
-    **id** is optional as it will be auto detected for this add-on instance.
+    to retrieve settings from it.**id** is optional as it will be auto detected
+    for this add-on instance.
 
     Example::
 
-        ..
         self.Addon = xbmcaddon.Addon()
         self.Addon = xbmcaddon.Addon('script.foo.bar')
-        ..
     """
     
     def __init__(self, id=None):
@@ -54,9 +51,7 @@ class Addon(object):
 
         Example::
 
-            ..
             locstr = self.Addon.getLocalizedString(32000)
-            ..
         """
         return u""
     
@@ -72,9 +67,71 @@ class Addon(object):
 
         Example::
 
-            ..
             apikey = self.Addon.getSetting('apikey')
-            ..
+        """
+        return ""
+    
+    def getSettingBool(self, id):
+        # type: (str) -> bool
+        """
+        Returns the value of a setting as a boolean. 
+
+        :param id: string - id of the setting that the module needs to access. 
+        :return: Setting as a boolean
+
+        New function added.
+
+        Example::
+
+            enabled = self.Addon.getSettingBool('enabled')
+        """
+        return True
+    
+    def getSettingInt(self, id):
+        # type: (str) -> int
+        """
+        Returns the value of a setting as an integer. 
+
+        :param id: string - id of the setting that the module needs to access. 
+        :return: Setting as an integer
+
+        New function added.
+
+        Example::
+
+            max = self.Addon.getSettingInt('max')
+        """
+        return 0
+    
+    def getSettingNumber(self, id):
+        # type: (str) -> float
+        """
+        Returns the value of a setting as a floating point number. 
+
+        :param id: string - id of the setting that the module needs to access. 
+        :return: Setting as a floating point number
+
+        New function added.
+
+        Example::
+
+            max = self.Addon.getSettingNumber('max')
+        """
+        return 0.0
+    
+    def getSettingString(self, id):
+        # type: (str) -> str
+        """
+        Returns the value of a setting as a unicode string. 
+
+        :param id: string - id of the setting that the module needs to access. 
+        :return: Setting as a unicode string
+
+        New function added.
+
+        Example::
+
+            apikey = self.Addon.getSettingString('apikey')
         """
         return ""
     
@@ -86,15 +143,90 @@ class Addon(object):
         :param id: string - id of the setting that the module needs to access. 
         :param value: string or unicode - value of the setting.
 
-        You can use the above as keywords for arguments.
+        You can use the above as keywords for arguments.**id** is optional
+        as it will be auto detected for this add-on instance.
 
         Example::
 
-            ..
             self.Addon.setSetting(id='username', value='teamkodi')
-            ..
         """
         pass
+    
+    def setSettingBool(self, id, value):
+        # type: (str, bool) -> bool
+        """
+        Sets a script setting. 
+
+        :param id: string - id of the setting that the module needs to access. 
+        :param value: boolean - value of the setting. 
+        :return: True if the value of the setting was set, false otherwise
+
+        You can use the above as keywords for arguments.
+
+        New function added.
+
+        Example::
+
+            self.Addon.setSettingBool(id='enabled', value=True)
+        """
+        return True
+    
+    def setSettingInt(self, id, value):
+        # type: (str, int) -> bool
+        """
+        Sets a script setting. 
+
+        :param id: string - id of the setting that the module needs to access. 
+        :param value: integer - value of the setting. 
+        :return: True if the value of the setting was set, false otherwise
+
+        You can use the above as keywords for arguments.
+
+        New function added.
+
+        Example::
+
+            self.Addon.setSettingInt(id='max', value=5)
+        """
+        return True
+    
+    def setSettingNumber(self, id, value):
+        # type: (str, float) -> bool
+        """
+        Sets a script setting. 
+
+        :param id: string - id of the setting that the module needs to access. 
+        :param value: float - value of the setting. 
+        :return: True if the value of the setting was set, false otherwise
+
+        You can use the above as keywords for arguments.
+
+        New function added.
+
+        Example::
+
+            self.Addon.setSettingNumber(id='max', value=5.5)
+        """
+        return True
+    
+    def setSettingString(self, id, value):
+        # type: (str, str_type) -> bool
+        """
+        Sets a script setting. 
+
+        :param id: string - id of the setting that the module needs to access. 
+        :param value: string or unicode - value of the setting. 
+        :return: True if the value of the setting was set, false otherwise
+
+        You can use the above as keywords for arguments.
+
+        New function added.
+
+        Example::
+
+            self.Addon.setSettingString(id='username', value='teamkodi')
+        """
+        return True
     
     def openSettings(self):
         # type: () -> None
@@ -103,9 +235,7 @@ class Addon(object):
 
         Example::
 
-            ..
             self.Addon.openSettings()
-            ..
         """
         pass
     
@@ -115,22 +245,19 @@ class Addon(object):
         Returns the value of an addon property as a string. 
 
         :param id: string - id of the property that the module needs to access.
+            Choices for the property are:
 
-        Choices for the property are:
-
-        =======  ==========  ============  ===========
-        author   changelog   description   disclaimer 
-        fanart   icon        id            name       
-        path     profile     stars         summary    
-        type     version                              
-        =======  ==========  ============  ===========
+        ======= ========== ============ ===========
+        author  changelog  description  disclaimer 
+        fanart  icon       id           name       
+        path    profile    stars        summary    
+        type    version                            
+        ======= ========== ============ ===========
 
         :return: AddOn property as a string
 
         Example::
 
-            ..
             version = self.Addon.getAddonInfo('version')
-            ..
         """
         return ""
